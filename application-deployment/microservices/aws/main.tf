95fab9d112c7a969cdc921dd7567b25d21d97928
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2019-07-23 17:16:15 -0400

    Update main.tf

--- application-deployment/microservices/aws/main.tf
+++ application-deployment/microservices/aws/main.tf
@@ -6,6 +6,10 @@ provider "vault" {
   address = "${var.vault_url}"
 }
 
+provider "nomad" {
+  address = "http://${element(module.nomadconsul.primary_server_public_ips, 0)}:4646"
+} 
+
 data "vault_generic_secret" "aws_auth" {
   path = "aws-tf/creds/deploy"
 }
