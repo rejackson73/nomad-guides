3dc576959829f479db89ea888485e0db96a96a8e
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2019-07-24 11:33:12 -0400

    Update vpc.tf
    Inserted '=' to first to tags

--- application-deployment/microservices/aws/vpc.tf
+++ application-deployment/microservices/aws/vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "sockshop" {
   cidr_block = "${var.vpc_cidr}"
   enable_dns_hostnames = true
 
-  tags {
+  tags = {
     Name = "${var.name_tag_prefix} VPC"
   }
 }
@@ -12,7 +12,7 @@ resource "aws_vpc" "sockshop" {
 resource "aws_internet_gateway" "sockshop" {
   vpc_id = "${aws_vpc.sockshop.id}"
 
-  tags {
+  tags = {
     Name = "${var.name_tag_prefix} IGW"
   }
 }
