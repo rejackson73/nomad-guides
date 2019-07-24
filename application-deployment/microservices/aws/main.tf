bc078da42aa1e06b73669a86f6e0eedc236ad107
rejackson73 <28791872+rejackson73@users.noreply.github.com>
2019-07-24 13:45:17 -0400

    Update main.tf

--- application-deployment/microservices/aws/main.tf
+++ application-deployment/microservices/aws/main.tf
@@ -7,7 +7,7 @@ provider "vault" {
 }
 
 provider "nomad" {
-  address = "http://${module.nomadconsul.primary_server_private_ips[0]}:4646"
+  address = "http://\"element(module.nomadconsul.primary_server_public_ips, 0)\":4646"
 }
 
 data "vault_generic_secret" "aws_auth" {
