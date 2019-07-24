fef5341bfd47c7e41b1cf239a2e0eaa845c3d5bf
rejackson73 <28791872+rejackson73@users.noreply.github.com>
2019-07-24 13:24:53 -0400

    Update outputs.tf
    modifying joins

--- application-deployment/microservices/aws/outputs.tf
+++ application-deployment/microservices/aws/outputs.tf
@@ -1,10 +1,10 @@
 output "IP_Addresses" {
   value = <<CONFIGURATION
 
-Client public IPs: ${join(", ", module.nomadconsul.client_public_ips)}
-Client private IPs: ${join(", ", module.nomadconsul.client_private_ips)}
-Server public IPs: ${join(", ", module.nomadconsul.primary_server_public_ips)}
-Server private IPs: ${join(", ", module.nomadconsul.primary_server_private_ips)}
+Client public IPs: join(", ", module.nomadconsul.client_public_ips) 
+Client private IPs: join(", ", module.nomadconsul.client_private_ips)
+Server public IPs: join(", ", module.nomadconsul.primary_server_public_ips)
+Server private IPs: join(", ", module.nomadconsul.primary_server_private_ips)
 
 `ssh -i "${var.key_name}.pem" ubuntu@element(module.nomadconsul.primary_server_public_ips, 0)`
 
