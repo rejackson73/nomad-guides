c6bb5b8a9663d544b0c7153375b6f37d3e46ceb5
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2019-07-24 09:34:29 -0400

    Update main.tf

--- application-deployment/microservices/aws/main.tf
+++ application-deployment/microservices/aws/main.tf
@@ -47,6 +47,22 @@ module "nomadconsul" {
   vault_url         = "${var.vault_url}"
 }
   
+  
+resource "nomad_quota_specification" "default" {
+  name        = "default"
+  description = "Default quota for all services"
+
+  limits {
+    region = "global"
+
+    region_limit {
+      cpu       = 2499
+      memory_mb = 9500
+    }
+  }
+  depends_on = ["module.nomadconsul"]
+}
+  
 resource "null_resource" "start_sock_shop" {
   provisioner "remote-exec" {
     inline = [
